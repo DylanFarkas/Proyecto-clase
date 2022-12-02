d011bb9993f437fc936941b2c724b55424e84db1
Jhoan1 <jose.antonio.fernandez@correounivalle.edu.co>
2022-12-01 19:18:46 -0500

    aqui inclui un endpoint

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get('/consultar', (req, res) => {
   res.send("Hola gente");
 });
 
+
+
+app.get('/saludojose', (req, res) => {
+  res.send("hola gente de parte de jose");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
